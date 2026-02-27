8b00916153975b3cf4b738fcdcfe6d6da303f865
root <root@saturn-02.fractal.com>
2026-02-27 22:37:24 +0800

    fix: use explicit dayjs plugin .js imports for runtime

--- server/utils/time.ts
+++ server/utils/time.ts
@@ -1,7 +1,7 @@
 import dayjs from 'dayjs'
-import utc from 'dayjs/plugin/utc'
-import timezone from 'dayjs/plugin/timezone'
-import customParseFormat from 'dayjs/plugin/customParseFormat'
+import utc from 'dayjs/plugin/utc.js'
+import timezone from 'dayjs/plugin/timezone.js'
+import customParseFormat from 'dayjs/plugin/customParseFormat.js'
 
 let initialized = false
 
